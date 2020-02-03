3a3e6a175b2ae05dbac9922dc2e3ccdf6ae01061
Michael <mihail.ponomaryov@gmail.com>
2020-02-03 00:01:51 +0000

    price helper

---
+++ frontend/helpers/PriceHelper.php
@@ -0,0 +1,39 @@
+<?php
+namespace frontend\helpers;
+
+use yii\i18n\MessageFormatter;
+
+class PriceHelper
+{
+    public static function price2Str($price)
+    {
+        $whole = (int) $price;
+        $fractional = (int) (fmod($price, 1) * 100);
+
+        $patterns = [
+            'whole' => [
+                '{value, spellout}',
+                '{value, plural, =0{рублей} =1{рубль} one{рубль} few{рубля} many{рублей} other{рублей}}',
+            ],
+            'fractional' => [
+                '{value, spellout,%spellout-cardinal-feminine}',
+                '{value, plural, =0{копеек} =1{копейка} one{копейка} few{копейки} many{копеек} other{копеек}}',
+            ],
+        ];
+
+        $formatter = new MessageFormatter();
+        $result = [];
+
+        foreach (['whole', 'fractional'] as $value) {
+            foreach ($patterns[$value] as $pattern) {
+                $result[] = $formatter->format(
+                    $pattern,
+                    ['value' => $$value],
+                    'ru-RU'
+                );
+            }
+        }
+
+        return implode(' ', $result);
+    }
+}

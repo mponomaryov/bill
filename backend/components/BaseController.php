28e890c0e036ca4ac03783343149e99ccb2e1acc
Michael <mihail.ponomaryov@gmail.com>
2020-02-06 01:27:04 +0000

    backend base controller

---
+++ backend/components/BaseController.php
@@ -0,0 +1,55 @@
+<?php
+namespace backend\components;
+
+use yii\web\Controller;
+use yii\filters\AccessControl;
+use yii\filters\VerbFilter;
+
+/**
+ * BaseController implements common behaviors and actions
+ * for application controllers.
+ */
+class BaseController extends Controller
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'actions' => ['login', 'error'],
+                        'allow' => true,
+                    ],
+                    // All other actions
+                    [
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'delete' => ['POST'],
+                    'logout' => ['POST'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+        ];
+    }
+}

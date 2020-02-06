3c0e17b52a662ad4cb7368f76e6448532e7cb6c7
Michael <mihail.ponomaryov@gmail.com>
2020-02-06 04:11:40 +0000

    ItemController and its views

---
+++ backend/controllers/ItemController.php
@@ -0,0 +1,59 @@
+<?php
+namespace backend\controllers;
+
+use Yii;
+use yii\data\ActiveDataProvider;
+use yii\web\NotFoundHttpException;
+
+use common\models\Item;
+use backend\components\BaseController;
+
+/**
+ * ItemController implements the CRUD actions for Item model.
+ */
+class ItemController extends BaseController
+{
+    /**
+     * Lists all Item models.
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $dataProvider = new ActiveDataProvider([
+            'query' => Item::find(),
+        ]);
+
+        return $this->render('index', [
+            'dataProvider' => $dataProvider,
+        ]);
+    }
+
+    /**
+     * Displays a single Item model.
+     * @param integer $id
+     * @return mixed
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+
+    /**
+     * Finds the Item model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param integer $id
+     * @return Item the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = Item::findOne($id)) !== null) {
+            return $model;
+        }
+
+        throw new NotFoundHttpException('The requested page does not exist.');
+    }
+}

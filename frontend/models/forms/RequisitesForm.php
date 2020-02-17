c5be38c20cb5e762d012e9e776739e346d7f148a
Michael <mihail.ponomaryov@gmail.com>
2020-02-17 09:09:47 +0000

    move validator to separate file

--- frontend/models/forms/RequisitesForm.php
+++ frontend/models/forms/RequisitesForm.php
@@ -3,35 +3,11 @@ namespace frontend\models\forms;
 
 use Yii;
 use yii\base\Model;
-use yii\validators\Validator;
 
 use common\models\Bill;
 use common\models\Organization;
 use common\models\Item;
-
-class ItnLengthValidator extends Validator
-{
-    public $message = 'length must be 10 or 12 symbols';
-
-    public function validateAttribute($model, $attribute)
-    {
-        $length = mb_strlen($model->$attribute, 'UTF-8');
-
-        if (!in_array($length, [10, 12])) {
-            $this->addError($attribute, $this->message);
-        }
-
-    }
-
-    public function clientValidateAttribute($model, $attribute, $view)
-    {
-        $lengths = json_encode([10, 12]);
-        $message = json_encode($this->message, JSON_UNESCAPED_SLASHES | JSON_UNESCAPED_UNICODE);
-        return "if ($.inArray(value.length, $lengths) === -1) {
-            messages.push($message);
-        }";
-    }
-}
+use frontend\components\ItnLengthValidator;
 
 /**
  * Requisites form
